99907e3879fb0d94b19ddf23c6dd5c12e0a3bc2d
ABHINAV  SHARMA <as0673505@gmail.com>
2026-07-25 23:45:14 +0530

    Time: 15 ms (23.77%), Space: 46.4 MB (34.54%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> map=new HashMap<>();
+        for (char c:t.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        for (char c:s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)-1);
+            if (map.get(c)==0)map.remove(c);
+        }
+        return map.isEmpty();
+    }
+}
