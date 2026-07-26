8e21fb4cfd4f16179ba3e7f4e706da9a4329f03c
ABHINAV  SHARMA <as0673505@gmail.com>
2026-07-26 19:37:15 +0530

    Time: 19 ms (5.34%), Space: 48.3 MB (11.58%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -1,15 +1,16 @@
 class Solution {
     public int maximumProduct(int[] nums) {
         Arrays.sort(nums);
-        int pro=1;
-        int res=1;
-        for (int i=nums.length-1;i>nums.length-4;i--){
-            pro=pro*nums[i];
-        }
-        for (int i=0;i<2;i++){
-            res=res*nums[i];
-        }
-        res=res*nums[nums.length-1];
-        return Math.max(pro,res);
+        // int pro=1;
+        // int res=1;
+        // for (int i=nums.length-1;i>nums.length-4;i--){
+        //     pro=pro*nums[i];
+        // }
+        // for (int i=0;i<2;i++){
+        //     res=res*nums[i];
+        // }
+        // res=res*nums[nums.length-1];
+        int len=nums.length;
+        return Math.max(nums[0]*nums[1]*nums[len-1],nums[len-1]*nums[len-2]*nums[len-3]);
     }
 }
