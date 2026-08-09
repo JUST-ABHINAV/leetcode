fcd3fc19d6d54172318dbd925a862737e6bfebe3
ABHINAV  SHARMA <as0673505@gmail.com>
2026-08-09 12:05:31 +0530

    Time: 66 ms (5.16%), Space: 73.4 MB (5.16%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int a = 0;
+        int b = 0;
+        int total = 0;
+        int max = 0;
+
+        HashSet<Integer> set = new HashSet<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (b = 0; b < fruits.length; b++) {
+
+            set.add(fruits[b]);
+            map.put(fruits[b], map.getOrDefault(fruits[b], 0) + 1);
+
+            total++;
+
+            while (set.size() > 2) {
+
+                int fruit = fruits[a];
+
+                map.put(fruit, map.get(fruit) - 1);
+
+                if (map.get(fruit) == 0) {
+                    set.remove(fruit);
+                    map.remove(fruit);
+                }
+
+                a++;
+                total--;
+            }
+
+            max = Math.max(max, total);
+        }
+
+        return max;
+    }
+}
