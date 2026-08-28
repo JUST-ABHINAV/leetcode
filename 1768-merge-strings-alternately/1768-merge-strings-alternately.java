40834aedae32bc917380083a771131cbe35733f4
ABHINAV  SHARMA <as0673505@gmail.com>
2026-08-28 22:52:04 +0530

    Time: 1 ms (96.08%), Space: 43.2 MB (48.68%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int i=0;
+        int n=word1.length();
+        int m=word2.length();
+        StringBuilder sb=new StringBuilder();
+        if (n>m){
+            while (i<n){
+                if (i<n){
+                    sb.append(word1.charAt(i));
+                }
+                if (i<m){
+                    sb.append(word2.charAt(i));
+                }
+                i++;
+            }
+        }
+        else{
+            while (i<m){
+                if (i<n){
+                    sb.append(word1.charAt(i));
+                }
+                if (i<m){
+                    sb.append(word2.charAt(i));
+                }
+                i++;
+            }
+        }
+        return sb.toString();
+    }
+}
