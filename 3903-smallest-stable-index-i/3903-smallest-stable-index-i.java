60e4776deafafb123c593f735df58d9a881c2579
ABHINAV  SHARMA <as0673505@gmail.com>
2026-09-04 22:00:12 +0530

    Time: 1 ms (99.79%), Space: 46.1 MB (94.47%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -4,19 +4,21 @@ class Solution {
             return 0;
         }
         int n=nums.length;
-        int[] prefix=new int[nums.length];
+        // int[] prefix=new int[nums.length];
         int[] sufix=new int[nums.length];
-        prefix[0]=nums[0];
+        // prefix[0]=nums[0];
         sufix[n-1]=nums[n-1];
-        for (int i=1;i<nums.length;i++){
-            prefix[i]=Math.max(prefix[i-1],nums[i]);
-        }
+        // for (int i=1;i<nums.length;i++){
+        //     prefix[i]=Math.max(prefix[i-1],nums[i]);
+        // }
         for (int i=n-2;i>=0;i--){
             sufix[i]=Math.min(sufix[i+1],nums[i]);
         }
+        int max=0;
         for (int i=0;i<n;i++){
-            if (prefix[i]-sufix[i]<=k){
-                System.out.print(prefix[i]+"\n"+sufix[i]);
+            max=Math.max(max,nums[i]);
+            if (max-sufix[i]<=k){
+                // System.out.print(prefix[i]+"\n"+sufix[i]);
                 return i;
                 
             }
