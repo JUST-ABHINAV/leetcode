d0c447b40032db10383aa6687d9cac5e86ea7711
ABHINAV  SHARMA <as0673505@gmail.com>
2026-09-05 13:02:27 +0530

    Time: 3 ms (100%), Space: 133.1 MB (32.44%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        if (nums.length==1){
+            return 0;
+        }
+        int n=nums.length;
+        // int[] prefix=new int[nums.length];
+        int[] sufix=new int[nums.length];
+        // prefix[0]=nums[0];
+        sufix[n-1]=nums[n-1];
+        // for (int i=1;i<nums.length;i++){
+        //     prefix[i]=Math.max(prefix[i-1],nums[i]);
+        // }
+        for (int i=n-2;i>=0;i--){
+            sufix[i]=Math.min(sufix[i+1],nums[i]);
+        }
+        int max=0;
+        for (int i=0;i<n;i++){
+            max=Math.max(max,nums[i]);
+            if (max-sufix[i]<=k){
+                // System.out.print(prefix[i]+"\n"+sufix[i]);
+                return i;
+                
+            }
+        }
+        return -1;
+    }
+}
